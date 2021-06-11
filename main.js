5c632743d3e85401773aa04aa374a73ffa09cd13
nadirg2 <gulievnadir3@gmail.com>
2021-06-11 23:35:01 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,108 @@
+particlesJS(
+  "particles-js",
+  {
+    particles: {
+      number: {
+        value: 40,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: "#fc1fff",
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#fc1fff",
+        },
+        polygon: {
+          nb_sides: 5,
+        },
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 8,
+          opacity_min: 0.1,
+          sync: false,
+        },
+      },
+      size: {
+        value: 6,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 80,
+          size_min: 2,
+          sync: false,
+        },
+      },
+      line_linked: {
+        enable: true,
+        distance: 200,
+        color: "#feabff",
+        opacity: 0.5,
+        width: 2,
+      },
+      move: {
+        enable: true,
+        speed: 7,
+        direction: "none",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        bounce: true,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse",
+        },
+        onclick: {
+          enable: false,
+          mode: "push",
+        },
+        resize: true,
+      },
+      modes: {
+        grab: {
+          distance: 800,
+          line_linked: {
+            opacity: 1,
+          },
+        },
+        bubble: {
+          distance: 800,
+          size: 80,
+          duration: 2,
+          opacity: 0.8,
+          speed: 3,
+        },
+        repulse: {
+          distance: 200,
+          duration: 8,
+        },
+        push: {
+          particles_nb: 4,
+        },
+        remove: {
+          particles_nb: 2,
+        },
+      },
+    },
+    retina_detect: true,
+  }
+);
